9f5577a3609fb92830349564d7f630158e104657
SapSanshik <104336376+SapSanshik@users.noreply.github.com>
2022-06-27 23:13:23 +0300

    fada-slider

--- scripts/sidebar-menu.js
+++ scripts/sidebar-menu.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+(function() {
+    const openingBtn = document.querySelector(".sidebar__hamburger");
+    const closingBtn = document.querySelector(".sidebar__close");
+    const sidebar = document.querySelector(".sidebar");
+
+
+    openingBtn.addEventListener("click", function() {
+        sidebar.classList.add("sidebar--opened")
+    });
+
+    function opensidebar() {
+    sidebar.classList.add("sidebar--opened")
+    };
+
+    closingBtn.addEventListener('click', function() {
+        sidebar.classList.remove('sidebar--opened');
+    });
+})();
+
